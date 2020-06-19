b4d00154a61c6f8c9614c067104d985418fd53b9
willem <willem@leit.so>
2020-06-19 16:53:47 +0200

    fix guireset command

--- src/main/java/toast/client/commands/cmds/GuiReset.kt
+++ src/main/java/toast/client/commands/cmds/GuiReset.kt
@@ -11,6 +11,7 @@ import toast.client.utils.Logger
 class GuiReset : Command("GuiReset", "${ToastClient.cmdPrefix}guireset", "Makes ClickGui fit on screen as much as possible", false, "guireset", "gr", "fixgui") {
     override fun run(args: Array<String>) {
         ClickGuiPositions.positions.clear()
+        ClickGuiPositions().writePositions()
         Logger.message("ClickGui has been reset.", Logger.INFO, true)
     }
 }
